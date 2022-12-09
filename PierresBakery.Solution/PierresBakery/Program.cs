a6d46350006bfd3cd79157b5c376459705c4263f
Robert Onstott <roonstott@gmail.com>
2022-12-09 15:28:08 -0800

    add UI to collect customer order quantities and print an order message reiterating quantities and telling price.

--- PierresBakery.Solution/PierresBakery/Program.cs
+++ PierresBakery.Solution/PierresBakery/Program.cs
@@ -13,8 +13,10 @@ namespace Interface
       string orderAnswer = Console.ReadLine().ToLower();
       if (orderAnswer == "y")
       {
-        Program.TakeOrder();
-
+        int[] order = Program.TakeOrder();
+        Bread breadOrder = new Bread(order[0]);
+        Pastry pastryOrder = new Pastry(order[1]);
+        Program.PrintOrder(breadOrder, pastryOrder);
       }
       else
       {
@@ -22,18 +24,28 @@ namespace Interface
       }
     }
 
-    public static void TakeOrder()
+    public static int[] TakeOrder()
     {
       Console.WriteLine("Bread is $5.00 per loaf. Buy two and the 3rd loaf is FREE"); 
       Console.WriteLine("How many loaves of bread would you like to buy?"); 
-      int bread = int.Parse(Console.ReadLine());
-      Bread breadOrder = new Bread(bread);
-
+      int bread = int.Parse(Console.ReadLine());     
       Console.WriteLine("Pastries are $2.00 each. Buy two and the third one is just $1.00 (3 for $5.00");
       Console.WriteLine("How many pastries would you like to buy?"); 
       int pastries = int.Parse(Console.ReadLine());
-      Pastry pastryOrder = new Pastry(pastries);
+      int[] order = {bread, pastries};
+      return order;
     }
 
+    public static void PrintOrder(Bread bread, Pastry pastry)
+    {
+      string breadWord = bread.LoafOrLoaves();
+      string pastryWord = pastry.PastryOrPastries();
+      string breadQuant = bread.NumberOfBread.ToString();
+      string pastryQuant = pastry.NumberOfPastry.ToString();
+      int totalPrice = pastry.ComputePrice() + bread.ComputePrice();
+      string stringPrice = totalPrice.ToString();
+      Console.WriteLine("You have ordered " + breadQuant + " " + breadWord + " of bread and " + pastryQuant + " " + pastryWord);
+      Console.WriteLine("The total price for your order is $" + stringPrice + ".00");
+    }
   }
 }
