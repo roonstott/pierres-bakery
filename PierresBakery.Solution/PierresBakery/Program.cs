488f9dd353132cb662601843fc3729bcad7e644e
Robert Onstott <roonstott@gmail.com>
2022-12-09 09:23:44 -0800

    Set up file structure

---
+++ PierresBakery.Solution/PierresBakery/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using ProjectName.Models;
+
+namespace Interface
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
